5a8180fde317b76777daaa3985736c40a84b31e4
gonzalo aime <aimegonzalo@gmail.com>
2026-03-02 15:03:33 +0000

    creando perfil nutricional

---
+++ app/controllers/nutritional_profiles_controller.rb
@@ -0,0 +1,12 @@
+class NutritionalProfilesController < ApplicationController
+  def show
+  end
+
+def new
+  @nutritional_profile = NutritionalProfile.new
+end
+
+  def edit
+  end
+
+end
---
+++ app/helpers/nutritional_profiles_helper.rb
@@ -0,0 +1,2 @@
+module NutritionalProfilesHelper
+end
