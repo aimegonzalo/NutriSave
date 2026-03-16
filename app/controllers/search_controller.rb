f997c75bd5486efeaa6919425e7885cf74056f36
gonzalo aime <aimegonzalo@gmail.com>
2026-03-16 13:21:22 +0000

    Basic Search function working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,9 @@
 class SearchController < ApplicationController
   def search
+    @results = []
+    if params[:search].present? && params[:search][:name].present?
+      @results = Product.where("name LIKE ?", "%#{params[:search][:name]}%")
+    end
   end
+
 end
