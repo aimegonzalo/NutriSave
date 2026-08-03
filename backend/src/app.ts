032df5057064e032195bb42cf8e9287a957e366a
Gonzalo Aime <aimegonzalo@gmail.com>
2026-08-03 16:12:02 -0300

    Back end - Added Cors options

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,20 @@
 import express, { type Express, type Request, type Response } from 'express';
 import dotenv from "dotenv";
 import userRoutes from "../routes/user";
+import cors from "cors"
 
 dotenv.config()
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: "http://localhost:5173"
+};
+
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 app.use("/api/users", userRoutes)
 
